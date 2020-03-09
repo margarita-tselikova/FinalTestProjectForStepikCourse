b3430840bf9e26da912a463935142312e7d83c64
Margarita Tselikova <margo.rita-kova@yandex.ru>
2020-03-09 19:12:40 +0300

    last style changes before review

--- pages/login_page.py
+++ pages/login_page.py
@@ -10,16 +10,13 @@ class LoginPage(BasePage):
         self.should_be_register_form()
 
     def should_be_login_url(self):
-        # реализуйте проверку на корректный url адрес
         current_url = self.browser.current_url
         assert "login" in current_url, "Url for login page doesn't contain 'login'"
 
     def should_be_login_form(self):
-        # реализуйте проверку, что есть форма логина
         assert self.is_element_present(*LoginPageLocators.LOGIN_FORM), "LOGIN_FORM is not presented"
 
     def should_be_register_form(self):
-        # реализуйте проверку, что есть форма регистрации на странице
         assert self.is_element_present(*LoginPageLocators.REGISTER_FORM), "REGISTER_FORM is not presented"
 
     def register_new_user(self, email, password):
